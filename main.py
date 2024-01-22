1e00f47024830d8c307e5afed9ebb101fc242c21
daniel3243gg <daniew600@gmail.com>
2024-01-22 17:24:12 -0300

    subindo correção

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 from comandos.comandosEspeciais import ComandosEspeciais
 from comandos.comandosJogos import Xadrez
+from comandos.funcoesUteis.funcoesUteisR import carregar_configuracoes
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -19,8 +20,16 @@ client = commands.Bot(command_prefix=commands.when_mentioned_or(prefixo), intent
   #         if filename != '__init__.py':
    #             await client.load_extension(f'comandos.{filename[:-3]}')
     # client.load_extension('comandosEspeciais')
-    
+async def setup(): 
+    config = await carregar_configuracoes()
+    client.config = config
 
+    # Eventos e comandos vão aqui
+    comandos_especiais = ComandosEspeciais(client)
+    await client.add_cog(comandos_especiais)
+
+    xadrez = Xadrez(client)
+    await client.add_cog(xadrez)
 
 @client.event
 async def on_ready():
@@ -31,17 +40,14 @@ async def on_ready():
     print('Prefixo:', prefixo)
     print('-----------------')
 
-    comandos_especiais = ComandosEspeciais(client)
-    await client.add_cog(comandos_especiais)
-    xadrez = Xadrez(client)
-    await client.add_cog(xadrez)
+
    
 
 @client.event
 async def on_message(message):
     # Não esqueça de adicionar isso para garantir que os comandos funcionem
     await client.process_commands(message)
-
+   
     #if message.content.lower().startswith('!bomdia'):
      #   await message.channel.send('ola mundo, bom dia!')
 
@@ -53,5 +59,8 @@ async def on_message(message):
 async def diga(ctx, *, mensagem):
     await ctx.send(mensagem)
 
-client.run('MTE4OTMzMDMyMDM5MTE0NzY2MA.GembW2.lKZTMlk9f--spS7Yx2eFK9DKlBorUJ0JB1dKWk')
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(setup())
+    client.run(client.config['token_bot'])
 
