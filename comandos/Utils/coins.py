56b07114b7883fdaf9ca4e974148ce3d53f238d9
daniel3243gg <daniew600@gmail.com>
2024-01-30 16:29:40 -0300

    documentando a classe coins

--- comandos/Utils/coins.py
+++ comandos/Utils/coins.py
@@ -2,6 +2,36 @@ from PDO import PDO
 import json
 from datetime import datetime
 
+"""Classe Coins
+
+    Returns:
+        classe que DEVE ser usado para manipular coins do usuario nela contem todos os metodos necessarios 
+        para consultar,debitar é creditar coins, TODA VEZ QUE INSTANCIA A CLASSE DEVE SER CHAMADO O METODO start
+        nele que cria o usuario no banco ou instancia ele com a classe. 
+
+        Metodos:
+        - __init__(self, user_id) : Construtor da Classe Coins, aqui ocorre a instancia com o banco.
+
+        
+        start: Metodo que sempre deve ser chamado juntamente com a classe, ele recebe o ID o usuario 
+        é verifica si ele ja existe na base de dados caso existir so coleta o ID dele no banco caso nao existir
+        ele cria o usuario é coleta o seu ID.
+
+        inserirCoins:  Metodo responsavel por adicionar as moedas ao usuario, recebe como parameto
+        a quantidade de moedas que vai ser inserido no banco, é tambem registra a movimentação na tabela registro_coins.
+
+        
+        removerCoins:    Metodo responsavel por remover as moedas do usuario,  recebe como parametro
+                         quantidade de moedas a ser removida é salva a movimentação negativada(EX: -10) indicando que foi 
+                         uma retirada na tabela registro_coins.
+
+                         
+        consultarCoins:    Metodo responsavel por retornar a quantidade de moedas do usuario ou o registro de 
+                           coins (caso passar True de parametro), ele retorna um INT da moedas do usuario ou uma lista
+                           de dicionarios contendo todas a movimentação do usuario caso for passado True de parametro.
+
+    
+"""
 class Coins :
     def __init__(self):
         self.user_id = None
@@ -49,6 +79,7 @@ class Coins :
             # Verifique se o registro_coin não é None antes de executar o segundo insertUpdate
             if self.registro_coin is not None:
                 self.conx.insertUpdate("INSERT INTO registro_coins (movement, coins_id, user_id) VALUES (?, ?, ?)", [qnt, self.registro_coin, self.user_id])
+               
                 return True
     def removerCoins(self, qnt:int=1):
         resul = self.conx.query("SELECT *FROM coins WHERE user_id = ? " , self.user_id)
@@ -72,13 +103,18 @@ class Coins :
 
             resp = self.conx.query("SELECT data_hora, movement FROM registro_coins WHERE user_id = ?", [self.user_id])
             json_resultante = json.dumps(resp, default=lambda o: o.isoformat() if isinstance(o, datetime) else None)
-
+            #acima estou transformando a data-hora do banco de dados em formato dicionario 
+            #o banco de dados envia um objeto datatime
             return json_resultante
         else:
             resp = self.conx.query("SELECT coins FROM coins WHERE user_id = ?", [self.user_id])
             resp = resp[0]['coins']
             return resp
-        
+
+
+    #ABAIXO ESTA AS FUNÇOES PARA RETORNAR ERROS sendo getErroSql() erros da consulta ou do banco 
+    #getErroCoins() erro da classe.  
+    #setErros() usada para setar erros da classe si caso nao passsar o erro ele vai escrever uma mensagem simples de erro.
     def setErros(self,erro:str = 'ocorreu um erro na classe.'):
         self.erro += '<br>'+ erro + '<br>'
         return True
